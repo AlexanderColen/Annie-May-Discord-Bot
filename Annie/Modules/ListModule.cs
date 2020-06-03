a9f2917e15dba15421309468d4072c51fa2da707
Alexander Colen <alexanderc@live.nl>
2020-06-02 23:07:26 -0400

    Users are now able to be quoted as argument for fetching their list statistics.

--- Annie/Modules/ListModule.cs
+++ Annie/Modules/ListModule.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using AnnieMayDiscordBot.Models;
 using AnnieMayDiscordBot.ResponseModels;
+using Discord;
 using Discord.Commands;
 using MongoDB.Driver;
 
@@ -33,20 +34,14 @@ namespace AnnieMayDiscordBot.Modules
             // Check if the given int parameter is a Discord User ID (18 characters long).
             if (userId.ToString().Length == 18)
             {
-                // Look the user's anilist ID up in the database, if found replace the userId parameter with that.
-                IMongoDatabase db = _dbClient.GetDatabase("AnnieMayBot");
-                var usersCollection = db.GetCollection<DiscordUser>("users");
-                var filter = Builders<DiscordUser>.Filter.Eq("discordId", userId);
-                var users = usersCollection.Find(filter).ToList();
-
-                if (users.Count == 0)
+                long foundId = FetchAnilistIdFromDatabase((ulong)userId);
+                if (foundId == -1)
                 {
-                    // If the user doesn't exist, inform the requester and return out of the method.
                     await ReplyAsync("This filthy weeb isn't in the database.");
                     return;
                 }
 
-                userId = users[0].anilistId;
+                userId = foundId;
             }
 
             try
@@ -58,6 +53,51 @@ namespace AnnieMayDiscordBot.Modules
             {
                 await ReplyAsync("Sorry, I could not find this Anilist user.");
             }
+        }
+
+        [Command("user")]
+        [Summary("Find a user's statistics using their username.")]
+        [Alias("list", "userlist")]
+        public async Task GetAnimeListAsync([Remainder] IUser user)
+        {
+            try
+            {
+                long userId = FetchAnilistIdFromDatabase(user.Id);
+                if (userId == -1)
+                {
+                    await ReplyAsync("This filthy weeb isn't in the database.");
+                    return;
+                }
+                
+                UserResponse userResponse = await _aniListFetcher.FindUserStatisticsAsync(userId);
+                await ReplyAsync("", false, _embedUtility.BuildUserEmbed(userResponse.user));
+            }
+            catch (HttpRequestException)
+            {
+                await ReplyAsync("Sorry, I could not find this Anilist user.");
+            }
+        }
+
+        /// <summary>
+        /// Fetch the Anilist ID from the database by looking for the User with the Discord ID.
+        /// </summary>
+        /// <param name="discordId">The Discord ID of the User.</param>
+        /// <returns>The Anilist ID if found, otherwise -1.</returns>
+        private long FetchAnilistIdFromDatabase(ulong discordId)
+        {
+            // Look the user's anilist ID up in the database, if found replace the userId parameter with that.
+            IMongoDatabase db = _dbClient.GetDatabase("AnnieMayBot");
+            var usersCollection = db.GetCollection<DiscordUser>("users");
+            var filter = Builders<DiscordUser>.Filter.Eq("discordId", discordId);
+            var users = usersCollection.Find(filter).ToList();
+
+            // Return -1 if the user wasn't found.
+            if (users.Count == 0)
+            {
+                return -1;
+            }
+
+            return users[0].anilistId;
         }
     }
 }
