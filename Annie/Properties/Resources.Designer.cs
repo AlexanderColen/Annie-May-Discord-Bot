474382fe1b2c351efb0b9f649bd3f521565a62d3
Alexander Colen <alexanderc@live.nl>
2020-07-12 20:37:14 -0400

    Can now draw on an image. ( #9 )
    Emojis not supported (yet), but maybe that could be a future expansion?

--- Annie/Properties/Resources.Designer.cs
+++ Annie/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace AnnieMayDiscordBot.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap AnnieMaySign {
+            get {
+                object obj = ResourceManager.GetObject("AnnieMaySign", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] catface {
