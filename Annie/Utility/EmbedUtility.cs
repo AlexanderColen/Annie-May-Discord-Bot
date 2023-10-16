b73c6e47be181decdf2fdeac0ccf64f3c781606c
Alexander Colen <alexanderc@live.nl>
2023-10-16 14:41:28 -0400

    #104 - Separated PAUSED media from In Progress / Reading

--- Annie/Utility/EmbedUtility.cs
+++ Annie/Utility/EmbedUtility.cs
@@ -234,6 +234,7 @@ namespace AnnieMayDiscordBot.Utility
                 StringBuilder completedStringBuilder = new StringBuilder();
                 StringBuilder plannedStringBuilder = new StringBuilder();
                 StringBuilder inProgressStringBuilder = new StringBuilder();
+                StringBuilder pausedStringBuilder = new StringBuilder();
                 StringBuilder droppedStringBuilder = new StringBuilder();
                 StringBuilder notOnListStringBuilder = new StringBuilder();
                 StringBuilder repeatingStringBuilder = new StringBuilder();
@@ -263,7 +264,7 @@ namespace AnnieMayDiscordBot.Utility
                             break;
 
                         case EmbedMediaListStatus.Paused:
-                            inProgressStringBuilder.Append($"{embedMedia.DiscordName} [{embedMedia.Progress}] | ");
+                            pausedStringBuilder.Append($"{embedMedia.DiscordName} [{embedMedia.Progress}] | ");
                             break;
 
                         case EmbedMediaListStatus.Planning:
@@ -288,8 +289,9 @@ namespace AnnieMayDiscordBot.Utility
                             break;
                     }
                 }
-
+                
                 string inProgress = inProgressStringBuilder.ToString().TrimEnd(' ', '|');
+                string paused = pausedStringBuilder.ToString().TrimEnd(' ', '|');
                 string repeating = repeatingStringBuilder.ToString().TrimEnd(' ', '|');
                 string completed = completedStringBuilder.ToString().TrimEnd(' ', '|');
                 string dropped = droppedStringBuilder.ToString().TrimEnd(' ', '|');
@@ -300,6 +302,11 @@ namespace AnnieMayDiscordBot.Utility
                 if (inProgress.Length != 0)
                 {
                     stringBuilder.Append($"**In-Progress**: {inProgress}\n");
+                }
+
+                if (paused.Length != 0)
+                {
+                    stringBuilder.Append($"**Paused**: {paused}\n");
                 }
 
                 if (repeating.Length != 0)
